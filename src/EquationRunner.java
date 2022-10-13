2773417e7db42c602eb67e7ba0f63847f91c8bd1
BT_4N2_33 <zwu2542@bths.edu>
2022-10-13 11:49:39 -0400

    changed values of placements to correct ones

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -18,19 +18,19 @@ public class EquationRunner {
         String removeParentheses3 = coordinates2.substring(0, 1);
         String removeParentheses4 = coordinates2.substring(1, removeParentheses1.length() - 1);
         int d1Placement = coordinates2.indexOf(",");
-        String secondInteger = coordinates2.substring(removeParentheses2.length(), p1Placement - 1);
+        String secondInteger = coordinates2.substring(removeParentheses2.length(), d1Placement - 1);
         int converted2 = Integer.parseInt(secondInteger);
         //converts strings into integers
         //converts strings into integers
         String removeParentheses5 = coordinates1.substring(0, 1);
         String removeParentheses6 = coordinates1.substring(1, removeParentheses1.length() - 1);
         int f1Placement = coordinates1.indexOf(",");
-        String thirdInteger = coordinates1.substring(p1Placement, removeParentheses2.length() - 1);
+        String thirdInteger = coordinates1.substring(f1Placement, removeParentheses2.length() - 1);
         int converted3 = Integer.parseInt(thirdInteger);
         String removeParentheses7 = coordinates2.substring(0, 1);
         String removeParentheses8 = coordinates2.substring(1, removeParentheses1.length() - 1);
         int q1Placement = coordinates2.indexOf(",");
-        String fourthInteger = coordinates2.substring(removeParentheses2.length(), p1Placement - 1);
+        String fourthInteger = coordinates2.substring(removeParentheses2.length(), q1Placement - 1);
         int converted4 = Integer.parseInt(fourthInteger);
         //converts strings into integers
         LinearEquation p = new LinearEquation(converted1,converted2,converted3,converted4);
